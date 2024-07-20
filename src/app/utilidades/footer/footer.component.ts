a21f2532017f527a624b17aac78eb9705b0bcb4c
David Ramos Marín <davidslesp@gmail.com>
2024-07-20 14:43:44 -0600

    Correciones HF

--- src/app/utilidades/footer/footer.component.ts
+++ src/app/utilidades/footer/footer.component.ts
@@ -20,7 +20,8 @@ export class FooterComponent implements OnInit {
       {
         icon: PrimeIcons.MAP_MARKER,
         titulo: 'Ubicación',
-        descripcion: 'Av. Nacional S/N “El Paraje” \nSanta María Coapan, Tehuacán Pue.' 
+        descripcion: 'Av. Nacional S/N “El Paraje” \nSanta María Coapan, Tehuacán Pue.' ,
+        url: 'https://www.google.com/maps/place/ESCUELA+NORMAL+SUPERIOR+DE+TEHUACAN/@18.4200858,-97.4097719,371m/data=!3m1!1e3!4m13!1m7!3m6!1s0x85c5bda612ed8c1b:0xd82c8ededd29c301!2sNacional+Sur,+Sta+Mar%C3%ADa+Coapan,+Tehuac%C3%A1n,+Pue.!3b1!8m2!3d18.4223903!4d-97.4075494!3m4!1s0x85c5bda6a4c53a33:0x6f8831da6bc2a3e4!8m2!3d18.4202698!4d-97.4102086'
       },
       {
         icon: PrimeIcons.PHONE,
